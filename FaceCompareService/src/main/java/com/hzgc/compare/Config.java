be56a73b9fe31720c36d587ef071f1972aec8b40
cdb156 <15658067087@163.com>
2018-11-27 17:11:41 +0800

    update2

--- FaceCompareService/src/main/java/com/hzgc/compare/Config.java
+++ FaceCompareService/src/main/java/com/hzgc/compare/Config.java
@@ -1,7 +1,14 @@
 package com.hzgc.compare;
 
+
+import com.hzgc.compare.common.PropertiesUtil;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Properties;
+
 public class Config {
-    public static final String JOB_PATH = "/static_worker_job";
+    public static final String SERVICE_PATH = "/static_compare_service";
     public static String ES_CLUSTER_NAME;
     public static String ES_HOST;
     public static int ES_CLUSTER_PORT;
@@ -9,5 +16,36 @@ public class Config {
     public static int FILES_PER_THREAD;
     public static int SERVICE_ID;
     public static String FILE_PATH;
+    public static String ZK_ADDRESS;
+    public static String WORKER_ADDRESS;
 
+    static {
+        Properties prop = PropertiesUtil.getProperties();
+        ES_CLUSTER_NAME = prop.getProperty("es.cluster.name");
+        ES_HOST = prop.getProperty("es.hosts");
+        String sysVar = System.getProperty("es.hosts");
+        if(sysVar != null && sysVar.length() != 0){
+            ES_HOST = sysVar;
+        }
+        ES_CLUSTER_PORT = Integer.parseInt(prop.getProperty("es.cluster.port"));
+        SERVICE_NUM = Integer.parseInt(prop.getProperty("service.num"));
+        FILES_PER_THREAD = Integer.parseInt(prop.getProperty("files.per.thread"));
+        FILE_PATH = prop.getProperty("file.path");
+        ZK_ADDRESS = prop.getProperty("zookeeper.address");
+        sysVar = System.getProperty("zk.address");
+        if(sysVar != null && sysVar.length() != 0){
+            ZK_ADDRESS = sysVar;
+        }
+        WORKER_ADDRESS = getLocalIpAddress();
+    }
+
+    private static String getLocalIpAddress(){
+        try {
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            return ip;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- FaceCompareService/src/main/java/com/hzgc/compare/es/ElasticSearchClient.java
+++ FaceCompareService/src/main/java/com/hzgc/compare/es/ElasticSearchClient.java
@@ -3,10 +3,20 @@ package com.hzgc.compare.es;
 import com.hzgc.common.Person;
 import com.hzgc.common.util.es.ElasticSearchHelper;
 import com.hzgc.compare.Config;
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermsQueryBuilder;
+import org.elasticsearch.search.SearchHit;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 public class ElasticSearchClient {
     private static TransportClient esClient;
 
@@ -15,6 +25,72 @@ public class ElasticSearchClient {
     }
 
     public static List<Person> readFromEs(List<String> ids){
-        return null;
+        SearchRequestBuilder requestBuilder = esClient.prepareSearch("manageperson")
+                .setTypes("Person");
+        requestBuilder.setSize(500);
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        TermsQueryBuilder queryBuilder = QueryBuilders.termsQuery("_id", ids.toArray(new String[ids.size()]));
+        boolQueryBuilder.must(queryBuilder);
+        requestBuilder.setQuery(boolQueryBuilder);
+        SearchResponse response = requestBuilder.execute().actionGet();
+        SearchHit[] hits = response.getHits().getHits();
+        return analyseResult(hits);
+    }
+
+    private static List<Person> analyseResult(SearchHit[] hits){
+        List<Person> res = new ArrayList<>();
+        for(SearchHit hit : hits){
+            Person person = new Person();
+            Map<String, Object> hitMap = hit.getSourceAsMap();
+            person.setId(hit.getId());
+            if(hitMap.get("sfz") != null) {
+                person.setSfz((String) hitMap.get("sfz"));
+            }
+            if(hitMap.get("xm") != null){
+                person.setXm((String) hitMap.get("xm"));
+            }
+            if(hitMap.get("xb") != null){
+                person.setXb((String) hitMap.get("xb"));
+            }
+            if(hitMap.get("mz") != null) {
+                person.setMz((String) hitMap.get("mz"));
+            }
+            if(hitMap.get("sr") != null){
+                person.setSr((String) hitMap.get("sr"));
+            }
+            if(hitMap.get("ssssqx") != null) {
+                person.setSsssqx((String) hitMap.get("ssssqx"));
+            }
+            if(hitMap.get("jd") != null){
+                person.setJd((String) hitMap.get("jd"));
+            }
+            if(hitMap.get("mp") != null) {
+                person.setMp((String) hitMap.get("mp"));
+            }
+            if(hitMap.get("mlxz") != null) {
+                person.setMlxz((String) hitMap.get("mlxz"));
+            }
+            if(hitMap.get("csd") != null) {
+                person.setCsd((String) hitMap.get("csd"));
+            }
+            if(hitMap.get("cym") != null) {
+                person.setCym((String) hitMap.get("cym"));
+            }
+            if(hitMap.get("jg") != null) {
+                person.setJg((String) hitMap.get("jg"));
+            }
+            if(hitMap.get("tp") != null) {
+                person.setTp((String) hitMap.get("tp"));
+            }
+            if(hitMap.get("tzz") != null) {
+                person.setTzz((String) hitMap.get("tzz"));
+            }
+            if(hitMap.get("bittzz") != null) {
+                person.setBittzz((String) hitMap.get("bittzz"));
+            }
+            res.add(person);
+        }
+        log.info("The size of response is " + res.size());
+        return res;
     }
 }
